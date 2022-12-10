5545833bca3e0e37af24c33af89bb0eeb841aa56
Michael Dokunmu <Mike.Dokunmu@ocean.ibm.com>
2022-12-10 14:42:27 -0600

    change

--- resource/resource.tf
+++ resource/resource.tf
@@ -1,25 +1,46 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.0"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "tf-webapp-rg"
-  location = "eastus"
+# # Generate a random integer to create a globally unique name
+# resource "random_integer" "ri" {
+#   min = 10000
+#   max = 99999
+# }
+
+# fetch resource group
+data "azurerm_resource_group" "rg" {
+  name = "rg-kcns-web-poc-eastus-010"
+  # location            = "eastus"
 }
 
-resource "azurerm_service_plan" "asp" {
-  name                = "appserviceplan"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+# Create the Linux App Service Plan
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = "webapp-asp-010"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "P1v2"
+  sku_name            = "B1"
 }
 
+# Create the web app, pass in the App Service Plan ID
 resource "azurerm_linux_web_app" "webapp" {
-  name                = "webapp"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_service_plan.asp.location
-  service_plan_id     = azurerm_service_plan.asp.id
-
-  site_config {}
+  name                = "webapp-010"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.appserviceplan.id
+  https_only          = true
+  site_config {
+    minimum_tls_version = "1.2"
+  }
 }
